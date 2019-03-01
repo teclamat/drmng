1ffe7232a667cefcfdcd702e4bdaa0229804649b
Mateusz Tecław <teclamat@vbox.localdomain>
2019-03-01 20:56:30 +0100

    Fix array length acquisition

--- kong_ng.user.js
+++ kong_ng.user.js
@@ -318,7 +318,7 @@ function main() {
 
             static getRandText(textArray) {
                 if (textArray instanceof Array) {
-                    return textArray[DRMng.Util.getRand(textArray.length() - 1)];
+                    return textArray[this.getRand(textArray.length - 1)];
                 }
                 return textArray;
             }
